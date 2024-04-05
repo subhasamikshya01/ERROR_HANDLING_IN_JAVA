5c2d125db69fe47b1865f58db70011809b4ad6db
subhasamikshya01 <165682929+subhasamikshya01@users.noreply.github.com>
2024-04-05 16:59:05 +0530

    Add files via upload

---
+++ Q10.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Q10 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a no:- ");
+		Double n=sc.nextDouble();
+		double k;
+		try {
+			if(n%(Math.PI/2)!=0) {
+				k=(Math.sin(n)+Math.cos(n))/Math.tan(n);
+				System.out.println(k);
+			}
+			else {
+				throw new ArithmeticException();
+			}
+		}
+		catch(ArithmeticException e) {
+			System.out.println("Error:- The number inpted should not be multiple of PI/2");
+		}
+	}
+
+}
